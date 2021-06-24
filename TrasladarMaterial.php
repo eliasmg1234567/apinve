a18611b63d0b545872803817e27b94a575db5966
eliasmg12345 <randyofgod666@gmail.com>
2021-06-24 12:57:50 -0400

    -

--- TrasladarMaterial.php
+++ TrasladarMaterial.php
@@ -10,17 +10,15 @@
     require("conexionp.php"); //Importa el archivo de la conexion a la BD
     
 // revisar
-    pg_query($conexion, "INSERT INTO psg_salida VALUES($params->id_oficinaEntrada)");
-    pg_query($conexion, "INSERT INTO psg_entrada VALUES($params->id_oficinaSalida)");
-    pg_query($conexion, "INSERT INTO psg_movimiento VALUES($params->id_material,$params->id_oficinaEntrada,$params->id_oficinaSalida,'$params->cantidad','$params->motivo_movimiento','$params->tipo_movimiento','$params->fecha_movimiento')");
+    pg_query($conexion, "INSERT INTO psg_movimiento (id_material,id_entrada,id_salida,cantidad,motivo_movimiento,tipo_movimiento,fecha_movimiento) VALUES($params->id_material,$params->id_entrada,$params->id_salida,$params->cantidad,'$params->motivo_movimiento','$params->tipo_movimiento','$params->fecha_movimiento')");
 
     //Envio de informacion del JSON
     
     echo json_encode($response); // Muestra el json generado
     header('Content-Type: application/json');
 
-    pg_query($conexion, "UPDATE psg_almacen set cantidad=cantidad-$params->cantidad where id_oficina=$params->id_oficinaSalida AND id_material=$params->id_material");
-    pg_query($conexion, "UPDATE psg_almacen set cantidad=cantidad+$params->cantidad where id_oficina=$params->id_oficinaEntrada AND id_material=$params->id_material");
+    pg_query($conexion, "UPDATE psg_almacen set cantidad=cantidad-$params->cantidad where id_oficina=$params->id_salida AND id_material=$params->id_material");
+    pg_query($conexion, "UPDATE psg_almacen set cantidad=cantidad+$params->cantidad where id_oficina=$params->id_entrada AND id_material=$params->id_material");
 
     class Result {}
 
